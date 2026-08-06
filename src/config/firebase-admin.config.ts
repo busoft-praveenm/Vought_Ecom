98db21842edf07abdb3f63c917fec6ebcd037eb9
agiratech-praveenm <praveen.m@agiratech.com>
2026-08-06 19:09:30 +0530

    fixed firebase admin config issue

--- src/config/firebase-admin.config.ts
+++ src/config/firebase-admin.config.ts
@@ -1,12 +1,27 @@
 import * as admin from "firebase-admin";
-import serviceAccount from "../../firebase-service-account.json";
+import * as fs from 'fs';
+import * as path from 'path';
+
+let serviceAccount: admin.ServiceAccount | undefined;
+
+try {
+  const accountPath = path.resolve(__dirname, "../../firebase-service-account.json");
+  if (fs.existsSync(accountPath)) {
+    serviceAccount = JSON.parse(fs.readFileSync(accountPath, 'utf8'));
+  }
+} catch (error) {
+  console.warn("Could not load Firebase service account credentials locally", error);
+}
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(
-      serviceAccount as admin.ServiceAccount,
-    ),
-  });
+  if (serviceAccount) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } else {
+    // If running in production or docker without the file, it will rely on default GOOGLE_APPLICATION_CREDENTIALS
+    admin.initializeApp();
+  }
 }
 
 export default admin;
